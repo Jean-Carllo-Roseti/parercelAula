740df3172aeb0c2f8faecf94fc71e9531a126e14
jean carllo <jeanrosete04@gmail.com>
2023-12-21 11:48:13 -0300

    timer finalizado.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,29 @@
 AOS.init();
+
+const dataDoEvento = new Date("Dec 25, 2023 22:00:00");
+const timeStampDoEvento = dataDoEvento.getTime();
+
+const contaHoras = setInterval(function() {
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+
+    const distanciaDoEvento = timeStampDoEvento - timeStampAtual;
+
+    const diasEmMS = 1000 * 60 * 60 * 24;
+    const horasEmMS = 1000 * 60 * 60;
+    const minutosEmMs = 1000 * 60;
+    
+    const diasAteOEvento = Math.floor(distanciaDoEvento / diasEmMS);
+    const horasAteOEvento = Math.floor((distanciaDoEvento % diasEmMS) / horasEmMS);
+    const minutosAteOEvento = Math.floor((distanciaDoEvento % horasEmMS) / minutosEmMs);
+    const segundosAteOEvento = Math.floor((distanciaDoEvento % minutosEmMs) / 1000);
+
+    document.getElementById('contador').innerHTML = `${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m ${segundosAteOEvento}s`;
+
+    if (distanciaDoEvento < 0) {
+        clearInterval(contaHoras);
+        document.getElementById('contador').innerHTML = 'Evento expirado.';
+    }
+
+
+}, 1000);
